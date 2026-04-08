874c357de0251b68a22bf8e1f2fe44932aec962f
personalchinu999-collab <personalchinu999@gmail.com>
2026-04-08 17:42:15 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import Index from "./pages/Index.tsx";
 import NotFound from "./pages/NotFound.tsx";
-
+import BackgroundWrapper from "@/components/BackgroundWrapper";
 const queryClient = new QueryClient();
 
 const App = () => (
